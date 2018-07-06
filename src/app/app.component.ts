52d7a34d3ccbd7bb2acb65b9355faa93a563143c
Andreas <apoyiatzis@outlook.com>
2018-07-05 22:47:08 -0400

    added grain bg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import * as d3 from 'd3';
+
+declare const d3;
+declare const textures;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    this.setTextureBg();
+  }
+
+  private setTextureBg() {
+    const svg = d3.select('#bg-texture')
+      .append('svg');
+    const t = textures.circles()
+      .thicker();
+    svg.call(t);
+    svg.append("circle")
+      .style("fill", t.url());
+  }
 }
